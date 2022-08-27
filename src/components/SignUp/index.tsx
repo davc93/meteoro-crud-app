43ebb4c0b43c58e614def4f532384ba7a81dbbc4
Diego <davc93@gmail.com>
2022-08-27 10:21:11 -0400

    probando sign up

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -16,45 +16,69 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 console.log('hello firebase');
 function SignUp(props:any) {
+
+  const createUser = async (auth:any, email:string,password:string) => {
+
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(auth,email,password);
+      const user = userCredentials.user; 
+      console.log('userCredentials',userCredentials);
+      console.log('user',user);        
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+  const getData = (ev:Event) =>{
+    ev.preventDefault();
+    const email = document.getElementById('email1')
+    const password = document.getElementById('password1')
+    if(email){
+      let emailValue =  email as HTMLFormElement
+      emailValue = emailValue.value
+      console.log(emailValue)
+    }
+  }
+
   return (
-    <form className="flex flex-col gap-4">
-  <div>
-    <div className="mb-2 block">
-      <Label
-        htmlFor="email1"
-        value="Your email"
-      />
-    </div>
-    <TextInput
-      id="email1"
-      type="email"
-      placeholder="name@flowbite.com"
-      required={true}
-    />
-  </div>
-  <div>
-    <div className="mb-2 block">
-      <Label
-        htmlFor="password1"
-        value="Your password"
-      />
-    </div>
-    <TextInput
-      id="password1"
-      type="password"
-      required={true}
-    />
-  </div>
-  <div className="flex items-center gap-2">
-    <Checkbox id="remember" />
-    <Label htmlFor="remember">
-      Remember me
-    </Label>
-  </div>
-  <Button type="submit">
-    Submit
-  </Button>
-</form>
+    <form className="flex flex-col gap-4 w-60 lg:w-1/2" onSubmit={getData}>
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="email1"
+            value="Your email"
+          />
+        </div>
+        <TextInput
+          id="email1"
+          type="email"
+          placeholder="name@flowbite.com"
+          required={true}
+        />
+      </div>
+      <div>
+        <div className="mb-2 block">
+          <Label
+            htmlFor="password1"
+            value="Your password"
+          />
+        </div>
+        <TextInput
+          id="password1"
+          type="password"
+          required={true}
+        />
+      </div>
+      <div className="flex items-center gap-2">
+        <Checkbox id="remember" />
+        <Label htmlFor="remember">
+          Remember me
+        </Label>
+      </div>
+      <Button type="submit">
+        Submit
+      </Button>
+    </form>
 
   )
 }
