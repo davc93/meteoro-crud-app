710fb3cd0b3333f1d34fac3b14a84f0cdde160a1
Diego <davc93@gmail.com>
2022-08-30 13:15:03 -0400

    se anaden modelos

---
+++ src/api.tsx
@@ -0,0 +1,26 @@
+import { Project } from "./models/Project";
+import { Service } from "./models/Service";
+
+export function getProjects(){
+
+}
+export function getServices(){
+
+}
+
+// parametros
+export function getProject(id:string){
+
+}
+export function getService(id:string){
+
+}
+
+export function addService(Service: Service){
+
+}
+
+export function addProject(Project: Project){
+
+}
+
---
+++ src/models/Project.tsx
@@ -0,0 +1,10 @@
+export interface Project {
+
+    id:string,
+    name:string,
+    description:string,
+    featureImage:string,
+    technologies: string[],
+    reference:string
+
+}
