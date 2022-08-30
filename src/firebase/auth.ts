00439d5d497c37b7ad816d0f57ae34f5a8ae3f5f
Diego <davc93@gmail.com>
2022-08-30 16:03:18 -0400

    se trabaja en modularizar authenticacion, falta agregar autenticacion de google, facebook ,twitter y github

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,22 +1,49 @@
-class Auth {
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+export class Auth {
 
     // Standar configuration
-    signUpEmailandPassword(){
+    async createUserWithEmailandPassword(email: string, password: string) {
+        const auth = getAuth();
+        try {
+            const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredentials.user;
+            console.log('userCredentials', userCredentials);
+            console.log('user', user);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async authEmailandPassword(email: string, password: string) {
+
+        const auth = getAuth();
+        try {
+            const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredentials.user;
+            console.log('userCredentials', userCredentials);
+            console.log('user', user);
+        } catch (error) {
+            console.log(error);
+        }
 
     }
-    authEmailandPassword(){
+    authGoogle() {
 
     }
-    authGoogle(){
+    authFacebook() {
 
     }
-    authFacebook(){
+    authGithub() {
 
     }
-    authGithub(){
-
-    }
-    logOut(){
+    logOut() {
+        const auth = getAuth();
+        signOut(auth).then(() => {
+            console.log(`Sign-out successful.`)
+        }).catch((error) => {
+            console.log(`An error happened.`);
+            console.log(error);
+        });
 
     }
 
